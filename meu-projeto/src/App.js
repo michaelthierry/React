7debcea01c1514ab5d21b3a86442d244979a9633
Michael Thierry <michaelthierry86@gmail.com>
2024-05-31 15:46:28 -0300

    build: make first components

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import './components/HelloWorld'
+import HelloWorld from './components/HelloWorld';
 
 function App() {
+  const nome = "Michael";
+
+  function soma(a, b){
+    return a + b
+  }
+
   return (
     <div className="App">
-      <h1>Olá react</h1>
+      <h2>Alterando o JSX</h2>
+      <p>Olá {nome}, seja bem vindo</p>
+      <p>Soma: {soma(1, 3)}</p>
+      <HelloWorld/>
     </div>
   );
 }
---
+++ meu-projeto/src/components/Frase.js
@@ -0,0 +1,13 @@
+
+function Frase() {
+
+    return(
+        <div>
+            <p>
+                Minha frase em componente
+            </p>
+        </div>
+    )
+}
+
+export default Frase
