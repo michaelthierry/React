05e7d99147dd3dff183d01038305adfd675cdd8b
Michael Thierry <michaelthierry86@gmail.com>
2024-06-01 12:09:19 -0300

    feat: add prop event

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -8,8 +8,7 @@ function App() {
   return (
     <div className="App">
       <h2>Testando evento</h2>
-      <Evento numero="1"/>
-      <Evento numero="2"/>
+      <Evento />
       <Form/>
     </div>
   );
--- meu-projeto/src/components/Evento.js
+++ meu-projeto/src/components/Evento.js
@@ -1,13 +1,21 @@
-function Evento({numero}) {
+import Button from './eventos/Button'
+
+function Evento() {
 
     function meuEvento(){
-        console.log(`Ativado! ${numero}`)
+        console.log(`Ativado! meu primeiro evento`)
+    
+    }
+
+    function segundoEvento(){
+        console.log(`Ativado! meu segundo evento`)
     }
 
     return (
         <div>
             <p>Clique para disparar o evento:</p>
-            <button onClick={meuEvento}>Ativar1</button>
+            <Button event={meuEvento} text='primeiro evento'/>
+            <Button event={segundoEvento} text='segundo evento'/>
         </div>
     )
 }
